87d9462c5bc9768b32cb3ba2fe80e98cf7929b6e
aarkay <rohan.k279@gmail.com>
2015-08-20 21:53:48 -0400

    More minor sprite spacing fixes

--- mtb.py
+++ mtb.py
@@ -661,8 +661,8 @@ def createNewThread(team1,team2,reqr,sub):
 			if getSprite(t1id) != '' and getSprite(t2id) != '':
 				t1sprite = getSprite(t1id)
 				t2sprite = getSprite(t2id)
-			body = '##**' + status + ':** ' + t1sprite + '[**' + t1 + '**](#bar-13-white)[**vs' 
-			body += '**](#bar-3-grey)[**' + t2 + '**](#bar-13-white)' + t2sprite + '\n\n--------\n\n'
+			body = '##**' + status + ':** ' + t1sprite + ' [**' + t1 + '**](#bar-13-white)[**vs' 
+			body += '**](#bar-3-grey)[**' + t2 + '**](#bar-13-white) ' + t2sprite + '\n\n--------\n\n'
 
 		else:
 			body = '**' + status + ": " +  t1 + ' vs ' + t2 + '**\n\n'
@@ -705,8 +705,8 @@ def createMatchInfo(team1,team2):
 			if getSprite(t1id) != '' and getSprite(t2id) != '':
 				t1sprite = getSprite(t1id)
 				t2sprite = getSprite(t2id)
-			body = '##**' + status + ':** ' + t1sprite + '[**' + t1 + '**](#bar-13-white)[**vs' 
-			body += '**](#bar-3-grey)[**' + t2 + '**](#bar-13-white)' + t2sprite + '\n\n--------\n\n'
+			body = '##**' + status + ':** ' + t1sprite + ' [**' + t1 + '**](#bar-13-white)[**vs' 
+			body += '**](#bar-3-grey)[**' + t2 + '**](#bar-13-white) ' + t2sprite + '\n\n--------\n\n'
 
 		else:
 			body = '**' + status + ": " +  t1 + ' vs ' + t2 + '**\n\n'
@@ -893,8 +893,8 @@ def updateScore(matchID, t1, t2, sub):
 			t1sprite = getSprite(t1id)
 			t2sprite = getSprite(t2id)
 	
-		text = '##**' + status + ':** ' + t1sprite + '[**' + t1 + '**](#bar-13-white)[**' + leftScore + '-' + rightScore 
-		text += '**](#bar-3-grey)[**' + t2 + '**](#bar-13-white)' + t2sprite + '\n\n' 
+		text = '##**' + status + ':** ' + t1sprite + ' [**' + t1 + '**](#bar-13-white)[**' + leftScore + '-' + rightScore 
+		text += '**](#bar-3-grey)[**' + t2 + '**](#bar-13-white) ' + t2sprite + '\n\n' 
 	else:
 		text = '**' + status + ": " +  t1 + ' ' + leftScore + '-' + rightScore + ' ' + t2 + '**\n\n'
 	if not goalUpdating:
